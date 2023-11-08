63cc067bdf51e3f4fc11d91f3a8d570a2818d36a
Andrey_Mazurevich <mazyrevich@gmail.com>
2023-11-08 22:48:44 +0300

    side nave

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,7 @@
+import "./ui/global.css";
+import { inter } from "./ui/fonts";
+import test from "node:test";
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +9,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
