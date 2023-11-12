d6d072430da163ca58c92ae2288e94131baff6b3
Andrey_Mazurevich <mazyrevich@gmail.com>
2023-11-12 23:47:33 +0300

    Changed admin user

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -34,9 +34,8 @@ export default function LoginForm() {
                 id="email"
                 type="email"
                 name="email"
-                placeholder="Enter your email address"
+                placeholder="your-email@mail.com"
                 required
-                defaultValue="user@nextmail.com"
               />
               <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
